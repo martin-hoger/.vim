1a69af8aa152def06783602b645703f570664743
Martin Höger <martin.hoger@gmail.com>
2015-04-17 10:54:38 +0200

    Text updated

--- bin/intall.sh
+++ bin/intall.sh
@@ -4,11 +4,11 @@ TARGET_DIR=~/.vim
 GIT_SOURCE="https://github.com/martin-hoger/.vim"
 
 if [ -d "$TARGET_DIR" ]; then
-  echo "\033[0;33mYou already have $TARGET_DIR present.\033[0m You'll need to remove it if you want to install"
+  echo "You already have $TARGET_DIR present. You'll need to remove it if you want to install"
   exit
 fi
 
-echo "\033[0;34mCloning from git...\033[0m"
+echo "Cloning from git..."
 hash git >/dev/null 2>&1 && env git clone "$GIT_SOURCE" "$TARGET_DIR" || {
   echo "git not installed"
   exit
