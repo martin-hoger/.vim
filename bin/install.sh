59498b1e5d8cab634dfcc3f7694180fb5e0329d7
Martin Höger <martin.hoger@gmail.com>
2016-07-26 14:16:12 +0200

    Install fonts

--- bin/install.sh
+++ bin/install.sh
@@ -21,6 +21,8 @@ ln -s $TARGET_DIR/vimrc .vimrc
 cd $TARGET_DIR
 git submodule init && git submodule update
 
+# Install patched-fonts
 # https://github.com/ryanoasis/nerd-fonts#font-installation
+echo "Installing patched fonts..."
 mkdir -p ~/.local/share/fonts
 cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20for%20Powerline%20Nerd%20Font%20Complete.otf
