f469c46254e9568f6669facd8921944c32b0c3a6
Martin Höger <martin.hoger@gmail.com>
2016-07-26 14:10:40 +0200

    Nerdtree fonts added

--- bin/install.sh
+++ bin/install.sh
@@ -20,3 +20,7 @@ ln -s $TARGET_DIR/vimrc .vimrc
 #Update submodules
 cd $TARGET_DIR
 git submodule init && git submodule update
+
+# https://github.com/ryanoasis/nerd-fonts#font-installation
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20for%20Powerline%20Nerd%20Font%20Complete.otf
